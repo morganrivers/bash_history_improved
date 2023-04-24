816bb9cf9f6aeb24841b272a0e511a9cb5e40159
morganrivers <73547769+morganrivers@users.noreply.github.com>
2023-04-24 16:45:39 +0200

    Update bash_history_backup.sh

--- bash_history_backup.sh
+++ bash_history_backup.sh
@@ -8,10 +8,12 @@ case "$1" in
         if [ -f "$backup_file" ]; then
             current_history_lines=$(wc -l < "$dmrivers_home/.bash_history")
             backup_history_lines=$(wc -l < "$backup_file")
-	    echo "current history lines"
-	    echo $current_history_lines
-            echo "backup history lines"
-            echo $backup_history_lines
+	    
+	    # uncomment to see line number counts if it pleases you
+	    #echo "current history lines"
+	    #echo $current_history_lines
+            #echo "backup history lines"
+            #echo $backup_history_lines
 
             if [ "$backup_history_lines" -gt "$current_history_lines" ]; then
                 cp "$backup_file" "$dmrivers_home/.bash_history"
