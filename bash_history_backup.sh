6d772a75b88c6e66893abc2ebf76209656abf8cb
morganrivers <73547769+morganrivers@users.noreply.github.com>
2023-05-31 13:53:09 +0100

    Update bash_history_backup.sh

--- bash_history_backup.sh
+++ bash_history_backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-dmrivers_home="/home/dmrivers"
-backup_file="$dmrivers_home/.bash_history_backup"
+backup_script_location="/usr/local/bin"
+backup_file="$HOME/.bash_history_backup"
 
 case "$1" in
     backup|restore)
         if [ -f "$backup_file" ]; then
-            current_history_lines=$(wc -l < "$dmrivers_home/.bash_history")
+            current_history_lines=$(wc -l < "$backup_script_location/.bash_history")
             backup_history_lines=$(wc -l < "$backup_file")
 	    
 	    # uncomment to see line number counts if it pleases you
@@ -16,12 +16,12 @@ case "$1" in
             #echo $backup_history_lines
 
             if [ "$backup_history_lines" -gt "$current_history_lines" ]; then
-                cp "$backup_file" "$dmrivers_home/.bash_history"
+                cp "$backup_file" "$backup_script_location/.bash_history"
             elif [ "$current_history_lines" -gt "$backup_history_lines" ]; then
-                cp "$dmrivers_home/.bash_history" "$backup_file"
+                cp "$backup_script_location/.bash_history" "$backup_file"
             fi
         else
-            cp "$dmrivers_home/.bash_history" "$backup_file"
+            cp "$backup_script_location/.bash_history" "$backup_file"
         fi
         ;;
     *)
