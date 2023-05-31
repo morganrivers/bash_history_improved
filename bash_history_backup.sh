b81217957c1a1cb250308b57fe6054e25a49239f
morganrivers <73547769+morganrivers@users.noreply.github.com>
2023-05-31 14:50:11 +0100

    Update bash_history_backup.sh

--- bash_history_backup.sh
+++ bash_history_backup.sh
@@ -5,23 +5,26 @@ backup_file="$HOME/.bash_history_backup"
 
 case "$1" in
     backup|restore)
-        if [ -f "$backup_file" ]; then
-            current_history_lines=$(wc -l < "$HOME/.bash_history")
-            backup_history_lines=$(wc -l < "$backup_file")
-	    
-	    # uncomment to see line number counts if it pleases you
-	    #echo "current history lines"
-	    #echo $current_history_lines
-            #echo "backup history lines"
-            #echo $backup_history_lines
+        if [ -f "$HOME/.bash_history" ]; then
+            if [ -f "$backup_file" ]; then
+                current_history_lines=$(wc -l < "$HOME/.bash_history")
+                backup_history_lines=$(wc -l < "$backup_file")
 
-            if [ "$backup_history_lines" -gt "$current_history_lines" ]; then
-                cp "$backup_file" "$HOME/.bash_history"
-            elif [ "$current_history_lines" -gt "$backup_history_lines" ]; then
+                if [ "$backup_history_lines" -gt "$current_history_lines" ]; then
+                    cp "$backup_file" "$HOME/.bash_history"
+                elif [ "$current_history_lines" -gt "$backup_history_lines" ]; then
+                    cp "$HOME/.bash_history" "$backup_file"
+                fi
+            else
                 cp "$HOME/.bash_history" "$backup_file"
             fi
         else
-            cp "$HOME/.bash_history" "$backup_file"
+            touch "$HOME/.bash_history"
+            if [ -f "$backup_file" ]; then
+                cp "$backup_file" "$HOME/.bash_history"
+            else
+                touch "$backup_file"
+            fi
         fi
         ;;
     *)
@@ -29,4 +32,3 @@ case "$1" in
         exit 1
         ;;
 esac
-
